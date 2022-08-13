ce8076ad8b349da5c131c17dc9b5679012f489e7
pchchv <ipchchv@gmail.com>
2022-08-13 09:12:24 +0300

    Implement getting a folder to load from a env file

--- src/main.go
+++ src/main.go
@@ -3,11 +3,34 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
-var URL string
+var (
+	URL        string
+	CurrentDir string
+)
+
+func init() {
+	// Load values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Panic("No .env file found")
+	}
+}
+
+func getEnvValue(v string) string {
+	// Getting a value. Outputs a panic if the value is missing.
+	value, exist := os.LookupEnv(v)
+	if !exist {
+		log.Panicf("Value %v does not exist", v)
+	}
+	return value
+}
 
 func main() {
+	CurrentDir = getEnvValue("DIR")
 	flag.StringVar(&URL, "url", "", "")
 	flag.Parse()
 	if URL == "" {
